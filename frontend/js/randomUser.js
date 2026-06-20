2f9bfb113dcc30072e4d80d0230aab9ab03add08
shahaana <24wh1a0553@bvrithyderabad.edu.in>
2026-06-20 10:33:41 +0530

    random user created

---
+++ frontend/js/randomUser.js
@@ -0,0 +1,18 @@
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var user = data.results[0];
+            var fullName = user.name.title + " " + user.name.first + " " + user.name.last;
+            document.getElementById("user-img").src=user.picture.large;
+            //name
+            document.getElementById("user-name").innerText=fullName;
+            //gender
+            document.getElementById("user-gender").innerText=user.gender;
+        })
+        .catch(function(err){
+            console.log("Error: " + err);
+        })
+}
